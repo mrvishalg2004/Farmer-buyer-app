649c17fbedc9a117ea1b2cbbb206fb21719cf86e
Vishal Golhar <135511723+mrvishalg2004@users.noreply.github.com>
2026-01-28 19:32:58 +0530

    update n/w issue

--- client/constants/config.ts
+++ client/constants/config.ts
@@ -3,5 +3,5 @@ import Constants from 'expo-constants';
 const localhost = Constants.expoConfig?.hostUri?.split(':')[0] || 'localhost';
 const LOCAL_SERVER = `http://${localhost}:5001`;
 
-// export const API_URL = 'https://farmer-buyer-app.onrender.com';
-export const API_URL = LOCAL_SERVER;
+export const API_URL = 'https://farmer-buyer-app.onrender.com';
+// export const API_URL = LOCAL_SERVER;
